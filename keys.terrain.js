6cd6c14f416f63b56a15323edc2e299cbe1a8793
adeyahya <adeyahyaprasetyo@gmail.com>
2023-12-08 11:22:30 +0700

    patch terrain

---
+++ keys.terrain.js
@@ -0,0 +1,8 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  pisco: {
+    mnemonic: "dilemma already city vintage club exotic stomach brand profit muffin truly art lion welcome park castle jungle offer genre emotion advice joy other begin",
+  },
+};
