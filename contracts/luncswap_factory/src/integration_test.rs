824b888c8faab6ae6bbf5c74ef2a3e77822d6b33
Ade Yahya <adeyahyaprasetyo@gmail.com>
2023-12-09 23:35:18 +0700

    fix: broken test

--- contracts/luncswap_factory/src/integration_test.rs
+++ contracts/luncswap_factory/src/integration_test.rs
@@ -269,22 +269,17 @@ fn instantiate() {
     let native_balance = get_native_balance(&owner, &router);
     assert_eq!(native_balance, Coin::new(1000 - 10, "lunc"));
 
-
-    // need to approve first 
-    router.execute_contract(swapper.clone(), cw20_token, &cw20_base::msg::ExecuteMsg::IncreaseAllowance {
-        spender: pair.contract_address.clone().into(),
-        amount: Uint128::from_str("100").unwrap(),
-        expires:None
-    }, &vec![]).unwrap();
-
-    // token need approval
-    router.execute_contract(
-        swapper,
-        Addr::unchecked(&pair.contract_address),
-        &luncswap_pair::msg::ExecuteMsg::Swap { 
-            input_token: luncswap_pair::msg::TokenSelect::Token1, 
-            input_amount: Uint128::from_str("1").unwrap(), 
-            min_output: Uint128::from_str("1").unwrap() },
-        &vec![]
-    ).unwrap();
+    // swap token
+    router
+        .execute_contract(
+            swapper,
+            Addr::unchecked(&pair.contract_address),
+            &luncswap_pair::msg::ExecuteMsg::Swap {
+                input_token: luncswap_pair::msg::TokenSelect::Token1,
+                input_amount: Uint128::new(1),
+                min_output: Uint128::zero(),
+            },
+            &coins(1, NATIVE_TOKEN_DENOM),
+        )
+        .unwrap();
 }
