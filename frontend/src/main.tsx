69496befe32710723a65810097548d5d96dbb984
Ade Yahya <adeyahyaprasetyo@gmail.com>
2023-12-10 02:48:21 +0700

    fix: terrain wallet-kit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { WalletProvider } from '@terra-money/wallet-kit'
+import { LCDClientConfig } from '@terra-money/feather.js'
+
+const piscoLCD: LCDClientConfig = {
+  lcd: "https://pisco-lcd.terra.dev",
+  chainID: "pisco-1",
+  gasPrices: { uluna: 0.015 },
+  gasAdjustment: 2,
+  prefix: "terra"
+}
+
+const defaultNetworks = { "pisco-1": piscoLCD }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WalletProvider defaultNetworks={defaultNetworks}>
+      <App />
+    </WalletProvider>
   </React.StrictMode>,
 )
