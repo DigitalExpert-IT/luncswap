93b5b18855c91c0fbfb9653fe5c11b82b921cbec
Y0SH4 <yosa.agung13@gmail.com>
2023-12-11 00:27:44 +0700

    inititate chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,27 @@
 import { useConnectedWallet, useWallet } from "@terra-money/wallet-kit"
+import { Button, Container, Box, Text, Code } from "@chakra-ui/react"
 
 function App() {
   const connectedWallet = useConnectedWallet();
-  const { connect, disconnect, availableWallets } = useWallet()
+  const { connect, disconnect, availableWallets, network, status } = useWallet()
 
   return (
-    <>
-      {availableWallets.map(item => (
-        <button onClick={() => connect(item.id)}>{item.name}</button>
-      ))}
-    </>
+    <Container maxW="container.xl" mt="2rem">
+      <Box>
+        {connectedWallet ? (
+          <Button onClick={() => disconnect()} colorScheme="red">Disconnect</Button>
+        ) : (
+          availableWallets.map(item => (
+            <Button onClick={() => connect(item.id)} disabled={!item.isInstalled} key={item.id} colorScheme="green">
+              Connect by {item.name}
+            </Button>
+          ))
+        )}
+        <Text mt="2rem">Network Name: {connectedWallet?.network} </Text>
+        <Text>Network Info:</Text>
+        <Code mt="1rem">{JSON.stringify({ network, status }, null, 2)}</Code>
+      </Box>
+    </Container>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { WalletProvider } from '@terra-money/wallet-kit'
-import { LCDClientConfig } from '@terra-money/feather.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { WalletProvider } from "@terra-money/wallet-kit";
+import { LCDClientConfig } from "@terra-money/feather.js";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const piscoLCD: LCDClientConfig = {
   lcd: "https://pisco-lcd.terra.dev",
   chainID: "pisco-1",
   gasPrices: { uluna: 0.015 },
   gasAdjustment: 2,
-  prefix: "terra"
-}
+  prefix: "terra",
+};
 
-const defaultNetworks = { "pisco-1": piscoLCD }
+const defaultNetworks = { "pisco-1": piscoLCD };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WalletProvider defaultNetworks={defaultNetworks}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </WalletProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
