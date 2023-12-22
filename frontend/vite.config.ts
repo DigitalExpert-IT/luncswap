b6c066e2bdf11c88b5e6565d6c0a7b02f85bb680
Y0SH4 <yosa.agung13@gmail.com>
2023-12-23 05:59:41 +0700

    dockerizing + change config port from package.json to vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
     react(),
     nodePolyfills({ globals: { Buffer: true } }),
   ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
+  },
   build: {
     sourcemap: false,
     rollupOptions: {
