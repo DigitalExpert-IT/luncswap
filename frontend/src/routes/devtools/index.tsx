ebfb37a640578df2bf7e16fab9510b191920dfe5
Y0SH4 <yosa.agung13@gmail.com>
2023-12-22 14:07:47 +0700

    add dtAddliquidity

--- frontend/src/routes/devtools/index.tsx
+++ frontend/src/routes/devtools/index.tsx
@@ -3,11 +3,22 @@ import Root from "./dtRoot";
 import React from "react";
 
 const TokenFactory = React.lazy(() => import("./dtTokenFactory"));
+const AddLiquidity = React.lazy(() => import("./dtAddLiquidity"));
 
 const routes: RouteObject = {
   path: "/devtools",
   element: <Root />,
-  children: [{ path: "/devtools", element: <TokenFactory /> }],
+  children: [
+    {
+      path: "/devtools",
+      element: (
+        <>
+          <TokenFactory />
+          <AddLiquidity />
+        </>
+      ),
+    },
+  ],
 };
 
 export default routes;
