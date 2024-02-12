cc252c75b4d377eecd5796614a924349ac389be8
Y0SH4 <yosa.agung13@gmail.com>
2024-02-12 09:03:29 +0700

    update

--- frontend/src/backend/worker.ts
+++ frontend/src/backend/worker.ts
@@ -5,7 +5,7 @@ import TxWrapper from "@/backend/cosmutils/TxWrapper";
 import CollectionTransaction from "@/backend/collection/transaction";
 
 const sleep = (ms: number) =>
-  new Promise<void>(resolve => {
+  new Promise(resolve => {
     setTimeout(resolve, ms);
   });
 
@@ -14,6 +14,7 @@ class Worker {
   private pairContractSet = new Set<string>();
   private factoryAddress = "";
   private txCol: CollectionTransaction;
+
   constructor(chainID: string, db: Db) {
     this.txCol = new CollectionTransaction(db);
     const lcdConfig = lcdConfigMap[chainID as "pisco-1"];
@@ -24,47 +25,61 @@ class Worker {
 
     this.factoryAddress = factoryContractAddress[chainID as "pisco-1"];
     if (!this.factoryAddress) {
-      throw new Error(`No Factory Contract on chain ${chainID}`);
+      throw Error(`No Factory Contract on chain ${chainID}`);
     }
 
-    this.initialize();
+    this.txCol.getPairList().then(pairList => {
+      for (const pairAddress of pairList) {
+        this.pairContractSet.add(pairAddress);
+      }
+
+      this.run();
+    });
   }
 
-  private async initialize() {
-    let blockHeight = await this.txCol.getLatestBlock();
+  private async getCurrentBlockHeight() {
+    const blockInfo = await this.lcd.tendermint.blockInfo();
+    return +blockInfo.block.header.height;
+  }
+
+  private async run() {
+    let blockHeight = (await this.txCol.getLatestBlock()) || 0;
     if (blockHeight === 0) {
       const contractHistory = await this.lcd.wasm.contractHistory(
         this.factoryAddress,
       );
-      blockHeight = contractHistory[0][0]?.updated?.block_height ?? 0;
+      blockHeight = contractHistory[0][0].updated?.block_height ?? 0;
     }
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      await this.processBlock(blockHeight);
-      await sleep(1000 * 60);
-      blockHeight++;
-    }
-  }
+      console.log("checking on block", blockHeight);
+      const txList = await this.lcd.tx.txInfosByHeight(blockHeight);
 
-  private async processBlock(blockHeight: number) {
-    console.log("Checking on block", blockHeight);
-    const txList = await this.lcd.tx.txInfosByHeight(blockHeight);
-    for (const tx of txList) {
-      console.log("Checking on tx", tx.txhash);
-      const txw = new TxWrapper(tx);
-      const pairInfo = txw.parsePairInfo(this.factoryAddress);
-      if (pairInfo) {
-        this.pairContractSet.add(pairInfo.contractAddress);
-        await this.txCol.insertPairInfo(pairInfo);
-      }
+      for (const tx of txList) {
+        console.log("checking on tx", tx.txhash);
+        const txw = new TxWrapper(tx);
+        const pairInfo = txw.parsePairInfo(this.factoryAddress);
+        if (pairInfo) {
+          this.pairContractSet.add(pairInfo.contractAddress);
+          await this.txCol.insertPairInfo(pairInfo);
+        }
 
-      for (const pairAddress of this.pairContractSet) {
-        const priceChangeInfo = txw.parsePriceChange(pairAddress);
-        if (priceChangeInfo) {
-          await this.txCol.insertPriceChangeInfo(priceChangeInfo);
+        for (const pairAddress of this.pairContractSet) {
+          const priceChangeInfo = txw.parsePriceChange(pairAddress);
+          if (priceChangeInfo) {
+            await this.txCol.insertPriceChangeInfo(priceChangeInfo);
+          }
         }
       }
+
+      const currentBlockHeight = await this.getCurrentBlockHeight();
+      if (currentBlockHeight <= blockHeight) {
+        console.log("sleeping for 1 minute");
+        await sleep(1000 * 60);
+      }
+
+      blockHeight++;
     }
   }
 }
